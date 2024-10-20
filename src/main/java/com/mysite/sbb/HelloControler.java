ab84602872c4f346face110c0e32bf2f11922d74
suvapor283 <suvapor8632@gmail.com>
2024-10-20 18:17:21 +0900

    1-4. spring boot develops, lombok 설치 & 테스트

--- src/main/java/com/mysite/sbb/HelloControler.java
+++ src/main/java/com/mysite/sbb/HelloControler.java
@@ -10,6 +10,6 @@ public class HelloControler {
     @GetMapping("/hello")  // ()의 URL요청이 발생하면 밑의 메서드가 실행  // 특정 URL경로를 서버의 특정 메서드와 연결
     @ResponseBody  // 메서드의 출력 결과가 문자열 그 자체임
     public String hello() {
-        return "Hello world";
+        return "Hello Spring Boot Board";
     }
 }
---
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor  // 해당 속성을 필요로 하는 생성자가 자동으로 생성
+@Getter  // final을 적용하면 속성값을 변경할 수 없기 때문에 @Setter는 의미가 없다.
+public class HelloLombok {
+    private final String hello;  // final은 뒤에 따라오는 자료형과 변수 등을 변경할 수 없게 만드는 키워드
+    private final int lombok;  // final이 없다면 생성자에 포함되지 않는다.
+
+    public static void main(String[] args){
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
+
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
+}
