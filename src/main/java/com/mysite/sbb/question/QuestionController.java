c69432c2420d8c4b63442078543d1bfd835993fa
suvapor283 <suvapor8632@gmail.com>
2024-10-28 23:51:53 +0900

    2-11. URL 프리픽스

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,16 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")  // URL 프리픽스
 @Controller
 @RequiredArgsConstructor  // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {  // Model 객체는 자바 클래스와 템플릿간의 연결고리 역할
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);  // Model 객체에 값을 담아 두면 템플릿에서 사용 가능
@@ -22,7 +24,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
