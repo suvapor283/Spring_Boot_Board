0730694006955ec6edef09d48593308a109a5444
suvapor283 <suvapor8632@gmail.com>
2024-12-20 19:26:13 +0900

    3-9. 답변 삭제 기능 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -77,4 +77,18 @@ public class AnswerController {
 
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+
+        this.answerService.delete(answer);
+
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
 }
