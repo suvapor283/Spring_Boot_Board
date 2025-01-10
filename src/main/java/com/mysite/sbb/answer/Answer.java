d241f117126d87890955572fcbe1f858d3c9d76c
suvapor283 <suvapor8632@gmail.com>
2025-01-10 22:17:21 +0900

    3-10. 질문 추천 기능 생성

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -29,4 +30,7 @@ public class Answer {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -33,4 +34,7 @@ public class Question {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
